60930c363da42dc7f16e37271b5b2428e12df006
Mahesh <mahesh.ashokit@gmail.com>
2024-11-19 22:42:28 +0530

    Reverted

--- 26_SpringData_Multiple_Databases_App/src/main/java/com/multiple/datasource/repository/payment/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.multiple.datasource.repository.payment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.multiple.datasource.entity.payment.PaymentDetails;
-
-public interface PaymentRepository extends JpaRepository<PaymentDetails, Long>{
-
-}
